33f0ca7412271464c595421888dc1a520f9ab373
Brian Añano <155237392+beuraieon-a@users.noreply.github.com>
2025-09-09 01:06:47 +0000

    Added

--- pages/1_⚠️ Advisories & Warnings.py
+++ pages/1_⚠️ Advisories & Warnings.py
@@ -274,9 +274,9 @@ with weather_tab:
     col1, col2 = st.columns(2)
 
     with col1:
+        legend_tstm = st.container(width = 600)
         legend_tstm.write('##### Thunderstorm')
-        legend_tstm = st.container(horizontal=True, horizontal_alignment='left', width = 600)
-        legend_tstm_content = legend_tstm.container(border=True)
+        legend_tstm_content = legend_tstm.container(horizontal=True, horizontal_alignment='left', border=True)
 
         legend_tstm_content_icon = legend_tstm_content.container(width=100)
         legend_tstm_content_icon.image('static/rainwarning_00_tstmadv.png')
@@ -285,9 +285,9 @@ with weather_tab:
         legend_tstm_content_text.write('Moderate to heavy rainshowers with possible isolated intense downpours, accompanied with lightning and strong winds, are ongoing or likely to occur within 1-2 hours.')
 
     with col2:
+        legend_tcws = st.container(width = 600)
         legend_tcws.write('##### Tropical cyclone winds')
-        legend_tcws = st.container(horizontal=True, horizontal_alignment='left', width = 600)
-        legend_tcws_content = legend_tcws.container(border=True)
+        legend_tcws_content = legend_tcws.container(horizontal=True, horizontal_alignment='left', border=True)
 
         legend_tcws_content_icon = legend_tcws_content.container(width=100)
         legend_tcws_content_icon.image('static/tcws1.png')
